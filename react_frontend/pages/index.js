828ee8b26042901bd360b288a765bdf8fda89a80
unknown <illiapopov2001@gmail.com>
2018-06-09 01:09:20 +0300

    test

--- react_frontend/pages/index.js
+++ react_frontend/pages/index.js
@@ -2,7 +2,6 @@ import ChatMessages from '../components/ChatMessages'
 import ChatForm from '../components/ChatForm'
 
 
-
 export default class extends React.Component {
     render() {
         return (
